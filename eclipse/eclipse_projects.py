28b740039c80b397e3af04e6ce292592c05d6b82
Alvaro del Castillo <acs@bitergia.com>
2014-05-07 06:01:36 +0200

    Use default COLLATE with utf8 (utf8_general_ci)

--- eclipse/eclipse_projects.py
+++ eclipse/eclipse_projects.py
@@ -433,7 +433,7 @@ def create_projects_schema(cursor):
             id varchar(255) NOT NULL,
             title varchar(255) NOT NULL,
             PRIMARY KEY (project_id)
-        ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8
     """
     project_repositories_table = """
         CREATE TABLE project_repositories (
@@ -441,14 +441,14 @@ def create_projects_schema(cursor):
             data_source varchar(32) NOT NULL,
             repository_name varchar(255) NOT NULL,
             UNIQUE (project_id, data_source, repository_name)
-        ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8
     """
     project_children_table = """
         CREATE TABLE project_children (
             project_id int(11) NOT NULL,
             subproject_id int(11) NOT NULL,
             UNIQUE (project_id, subproject_id)
-        ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8
     """
 
     # The data in tables is created automatically.
