0d1d4b52902a959de0220fcfc49e99ff6def8852
Alvaro del Castillo <acs@bitergia.com>
2014-12-22 13:36:15 +0100

    rjson installed manually in R. Specify mlstats version which works with automator.

--- vagrant/scripts/install-metrics-grimoire.sh
+++ vagrant/scripts/install-metrics-grimoire.sh
@@ -10,6 +10,9 @@ git clone https://github.com/MetricsGrimoire/RepositoryHandler.git
 git clone https://github.com/MetricsGrimoire/CVSAnalY.git
 git clone https://github.com/MetricsGrimoire/Bicho.git
 git clone https://github.com/MetricsGrimoire/MailingListStats.git
+cd  MailingListStats
+git checkout c6658738845471f6629aff4c84b96b0e3db8437d
+cd ..
 git clone https://github.com/MetricsGrimoire/IRCAnalysis.git
 git clone https://github.com/MetricsGrimoire/Octopus.git
 git clone https://github.com/MetricsGrimoire/Sibyl.git
--- vagrant/scripts/install-r-packages.sh
+++ vagrant/scripts/install-r-packages.sh
@@ -4,7 +4,6 @@ R_REPO="http://cran.us.r-project.org"
 
 cat << EOF > R.packages
 install.packages('RMySQL',repos='${R_REPO}')
-install.packages('rjson',repos='${R_REPO}')
 install.packages('RColorBrewer',repos='${R_REPO}')
 install.packages('ggplot2',repos='${R_REPO}')
 install.packages('rgl',repos='${R_REPO}')
@@ -12,7 +11,13 @@ install.packages('optparse',repos='${R_REPO}')
 install.packages('ISOweek',repos='${R_REPO}')
 install.packages('zoo',repos='${R_REPO}')
 EOF
+# install.packages('rjson',repos='${R_REPO}') not exists for R 3.0.2 from Ubuntu
 
 R '--vanilla' < R.packages
-
 rm R.packages
+
+# rjson
+wget http://cran.rstudio.com/src/contrib/Archive/rjson/rjson_0.2.13.tar.gz
+R CMD INSTALL rjson_0.2.13.tar.gz
+rm rjson_0.2.13.tar.gz
+
