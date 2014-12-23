a5f850952e00d61f1aedc5b98047652b4cd29d67
Alvaro del Castillo <acs@bitergia.com>
2014-12-23 16:59:04 +0100

    Improved scripts so we can reuse automator/02-config-github.sh in order to analyze new github repos.

--- vagrant/scripts/automator/02-config-github.sh
+++ vagrant/scripts/automator/02-config-github.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+source `dirname $0`/../variables.sh
+
 su - ${DASH_USER} << EOF
 cd Automator
 ./create_projects.py -p tests/github_test.conf -d tests -s -n GitHubTest  --dbuser=root --dbpasswd=${ROOT_DBPASSWD}
 sed -i tests/GitHubTest/conf/main.conf \
-    -e "s/db_password =[ ]*$/db_password = rootpw/" \
+    -e "s/db_password =[ ]*$/db_password = ${ROOT_DBPASSWD}/" \
     -e "s/# backend_user = miningbitergia/backend_user = ${GITHUB_USER}/" \
     -e "s/# backend_password = passwd/backend_password =  ${GITHUB_PASSWD}/"
 ./launch.py -d /home/automator/Automator/tests/GitHubTest
